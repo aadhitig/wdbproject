50072920479c6fd2c31f8c3817b76d6e9db5aa00
Aadhiti Ghankota <aadhitighankota@berkeley.edu>
2021-09-11 19:52:53 -0700

    finished

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 import express from 'express';
-var app = express();
+const app = express();
+app.use(express.json())
 import fetch from 'node-fetch';
 
-var newCats = []
-var newVeges = []
-var deletedVeges = []
+let newCats = []
+let newVeges = []
+let deletedVeges = []
 
-app.get('/', function (req, res) {
+app.get('/', async function (req, res) {
+    
+    async function getData(url) {
+        const response = await fetch(url);
+        return response.json();
+      }
+    
+    const cats = await getData('http://cats-in-the-sky.herokuapp.com/api/cats');
+    const veges = await getData('http://cats-in-the-sky.herokuapp.com/api/veges');
 
-    fetch('http://cats-in-the-sky.herokuapp.com/api/catss')
-    .then(res => res.json())
-    .then(json => {
-        console.log(json)
-    })
-    Matchings = {};
+    let Matchings = {};
+    
     for (let i = 0; i < cats.length; i++) {
-        veggies = [];
-        catLetter = cat[i].charAt(0);
+        let veggies = [];
+        let catLetter = cats[i].charAt(0);
         // veggies are from API
         for (let i = 0; i < veges.length; i++) {
             if (veges[i].startsWith(catLetter) && !deletedVeges.includes(veges[i])) {
@@ -28,29 +33,39 @@ app.get('/', function (req, res) {
                 veggies.push(veges[i]);
             }
         }
-        Matchings[cat[i]] = veggies;
+        Matchings[cats[i]] = veggies;
     }
-    return Matchings;
+    
+    res.send(Matchings);
   })
 
 app.post('/', function (req, res) {
-    if (res.keys()[0].equals("cat")) {
-        newCats.push(res[res.keys()[0]]);
+    if (req.body.cat) {
+        newCats.push(req.body.cat);
+        res.send("POST Complete")
+        return
     }
-    else if (res.keys()[0].equals("vege")) {
-        newVeges.push(res[res.keys()[0]]);
+    else if (req.body.vege) {
+        newVeges.push(req.body.vege);
+        res.send("POST Complete")
+        return
     }
   })
 
-// delete request body
-app.delete('/user', function (req, res) {
-    if (newVeggies.includes(res.vege)) {
-        const index = newVeggies.indexOf(res.vege);
-        newVeges.splice(index, 1)
+app.delete('/', async function (req, res) {
+    async function getData(url) {
+        const response = await fetch(url);
+        return response.json();
+      }
+    const veges = await getData('http://cats-in-the-sky.herokuapp.com/api/veges');
+    
+    if (newVeges.includes(req.body.vege[0])) {
+        newVeges = newVeges - req.body.vege
     }
-    else if (true/* new veggies in get request to api veges */) {
-        deletedVeges.push(res.vege)
+    else if (veges.includes(req.body.vege[0])) {
+        deletedVeges.push(req.body.vege[0])
     }
+    res.send("Delete Complete")
   })
 
 app.listen(3000, function() {
